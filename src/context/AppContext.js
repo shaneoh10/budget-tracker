34820b4ece78e0bb57675b0b278b9874131ef650
Shane O'Hanlon <shaneaghada@gmail.com>
2021-12-08 10:41:41 +0000

    Create context

---
+++ src/context/AppContext.js
@@ -0,0 +1,33 @@
+import { createContext, useReducer } from 'react';
+
+const AppReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+const initialState = {
+    budget: 2000,
+    expenses: [
+        {id: 12, name: 'shopping', cost: 50},
+        {id: 13, name: 'fuel', cost: 40},
+        {id: 14, name: 'clothes', cost: 150},
+    ]
+}
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return(
+        <AppContext.Provider value={{
+            budget: state.budget,
+            expenses: state.expenses,
+            dispatch
+        }}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
